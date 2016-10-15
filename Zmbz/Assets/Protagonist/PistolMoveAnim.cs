ae5966f915e4f0f242c84b7ed7c2f9c0e747f45c
Raahul <napalaniappan@paypal.com>
2016-10-15 15:22:53 -0700

    Muzzle Flash

--- Zmbz/Assets/Protagonist/PistolMoveAnim.cs
+++ Zmbz/Assets/Protagonist/PistolMoveAnim.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PistolMoveAnim : MonoBehaviour {
 	Animator anim;
+	[SerializeField]ParticleSystem mFlash;
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
@@ -10,22 +11,30 @@ public class PistolMoveAnim : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton ("Fire2")) {
+		
+		if (Input.GetButton ("Fire2") && !anim.GetCurrentAnimatorStateInfo (0).IsName ("Reload")) {
 			anim.SetBool ("Aiming", true);
+			Camera.main.fieldOfView = 48;
 		} 
 		else {
 			anim.SetBool ("Aiming", false);
+			Camera.main.fieldOfView = 60;
 		}
 
-		if (Input.GetButtonDown("Fire1")) {
+		if (Input.GetMouseButtonDown(0) && 
+			(!anim.GetCurrentAnimatorStateInfo (0).IsName ("ShootRelaxed") || !anim.GetCurrentAnimatorStateInfo (0).IsName ("ShootAim"))) {
 			anim.SetBool ("Shoot", true);
+			mFlash.Play ();
+
 		} else {
+			mFlash.Stop ();
 			anim.SetBool ("Shoot", false);
 		}
 
 
 
 		if (Input.GetKeyDown("r")) {
+			Camera.main.fieldOfView = 60;
 			anim.SetBool ("Reload", true);
 		} 
 		else {
--- Zmbz/Assets/Protagonist/WeaponSwitch.cs
+++ Zmbz/Assets/Protagonist/WeaponSwitch.cs
@@ -2,24 +2,27 @@
 using System.Collections;
 
 public class WeaponSwitch : MonoBehaviour {
+	Animator anim;
 	[SerializeField] GameObject axe; 
 	[SerializeField] GameObject pistol;
 	// Use this for initialization
 	void Start () {
-		
+		anim = pistol.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Q)) {
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Q)) {
 			if (axe.activeSelf) {
 				axe.SetActive (false);
 				pistol.SetActive (true);
+			} else {
+				if (!anim.GetCurrentAnimatorStateInfo (0).IsName ("Reload")) {
+					axe.SetActive (true);
+					pistol.SetActive (false);
+				}
 			} 
-			else {
-				axe.SetActive (true);
-				pistol.SetActive (false);
-			}
-		} 
+		}
 	}
 }
