d511dc9724560e06f26b4da92253d3766b8a2ace
jaewook0420 <jaewook042082@gmail.com>
2016-10-14 19:14:40 -0700

    Protagonist

---
+++ Protagonist/PistolMoveAnim.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PistolMoveAnim : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire2")) {
+			anim.SetBool ("Aiming", true);
+		} 
+		else {
+			anim.SetBool ("Aiming", false);
+		}
+
+		if (Input.GetButtonDown("Fire1")) {
+			anim.SetBool ("Shoot", true);
+		} else {
+			anim.SetBool ("Shoot", false);
+		}
+
+
+
+		if (Input.GetKeyDown("r")) {
+			anim.SetBool ("Reload", true);
+		} 
+		else {
+			anim.SetBool ("Reload", false);
+		}
+
+
+	}
+}
---
+++ Protagonist/WeaponSwitch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSwitch : MonoBehaviour {
+	[SerializeField] GameObject axe; 
+	[SerializeField] GameObject pistol;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			if (axe.activeSelf) {
+				axe.SetActive (false);
+				pistol.SetActive (true);
+			} 
+			else {
+				axe.SetActive (true);
+				pistol.SetActive (false);
+			}
+		} 
+	}
+}
